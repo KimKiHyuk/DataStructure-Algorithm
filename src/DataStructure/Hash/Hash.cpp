f1c8c2ef7179b1746ce422ff718af2d10ff704a8
Key Kim <keyhyuk.kim@gmail.com>
2019-02-20 13:55:00 +0000

    Add hash search

--- src/DataStructure/Hash/Hash.cpp
+++ src/DataStructure/Hash/Hash.cpp
@@ -21,8 +21,27 @@ void key_ds::Hash::hash_insert(key_ds::HashSet* data)
 	
 	this->hashTable[key].chain.push_back(data);
 	this->hashTable[key].count += 1;
+}
+
+key_ds::HashSet key_ds::Hash::hash_search(int key)
+{
+	key_ds::HashSet result;
 	
-	print_all();
+	std::list<key_ds::HashSet*>::iterator iter;
+	int _key = key_ds::Hash::get_hash_key(key);
+	
+	if (this->hashTable[_key].count > 0)
+	{
+		for (iter = this->hashTable[_key].chain.begin(); iter != this->hashTable[_key].chain.end(); iter++)
+		{
+			if ((*iter)->key == key)
+			{
+				result = *(*iter);
+			}
+		}
+	}
+	
+	return result;
 }
 
 int key_ds::Hash::get_hash_key(int key)
@@ -32,15 +51,15 @@ int key_ds::Hash::get_hash_key(int key)
 
 void key_ds::Hash::print_all()
 {
-	std::cout << " ===== print head =====" << std::endl;
+	std::cout << " ===== print start =====" << std::endl;
 	std::list<key_ds::HashSet*>::iterator iter;
 	
 	for (int i = 0; i < this->size; i++)
 	{
+		std::cout << "[ " << i << " ] linked list" << std::endl;
 		for (iter = this->hashTable[i].chain.begin(); iter != this->hashTable[i].chain.end(); iter++)
 		{
-			std::cout << "key : " << (*iter)->key << std::endl;
-			std::cout << "data : " << (*iter)->data << std::endl;
+			std::cout << "	key : " << (*iter)->key << " |  data : " << (*iter)->data << std::endl;
 		}
 	}
 	
