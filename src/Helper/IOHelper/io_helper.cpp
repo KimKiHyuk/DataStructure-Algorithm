0ff5ec88ccf03fbe2da7e05e5338a752e52d0784
Key Kim <keyhyuk.kim@gmail.com>
2019-03-13 14:38:55 +0000

    Initalize IO helper

---
+++ src/Helper/IOHelper/io_helper.cpp
@@ -0,0 +1,15 @@
+#include "io_helper.h"
+#include <fstream>
+#include <string>
+#include <iostream>
+
+
+io_helper::io_helper()
+{
+	std::cout << "aa" << std::endl;
+}
+
+io_helper::~io_helper()
+{
+	std::cout << "bb" << std::endl;
+}
